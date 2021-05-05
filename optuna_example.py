ccd8c38b840f8f85ec27b6839ee7ae9abf182ed7
Christian Michelsen <christianmichelsen@gmail.com>
2021-05-05 11:53:24 +0200

    Update to 2021 (include optuna)

---
+++ optuna_example.py
@@ -0,0 +1,50 @@
+import optuna
+from optuna.samplers import TPESampler
+from optuna.integration import LightGBMPruningCallback
+from optuna.pruners import MedianPruner
+import lightgbm as lgb
+
+
+def objective(trial):
+
+    boosting_types = ["gbdt", "rf", "dart"]
+    boosting_type = trial.suggest_categorical("boosting_type", boosting_types)
+
+    params = {
+        "objective": "binary",
+        "boosting": boosting_type,
+        "verbosity": -1,
+        "max_depth": trial.suggest_int("max_depth", 2, 63),
+        "min_child_weight": trial.suggest_loguniform("min_child_weight", 1e-5, 10),
+        "scale_pos_weight": trial.suggest_uniform("scale_pos_weight", 10.0, 30.0),
+    }
+
+    N_iterations_max = 10_000
+    early_stopping_rounds = 50
+
+    if boosting_type == "dart":
+        N_iterations_max = 100
+        early_stopping_rounds = None
+
+    cv_res = lgb.cv(
+        params,
+        lgb_data_train,
+        num_boost_round=N_iterations_max,
+        early_stopping_rounds=early_stopping_rounds,
+        verbose_eval=False,
+        seed=42,
+        callbacks=[LightGBMPruningCallback(trial, "auc")],
+    )
+
+    num_boost_round = len(cv_res["auc-mean"])
+    trial.set_user_attr("num_boost_round", num_boost_round)
+    return cv_res["auc-mean"][-1]
+
+
+study = optuna.create_study(
+    direction="maximize",
+    sampler=TPESampler(seed=42),
+    pruner=MedianPruner(n_warmup_steps=50),
+)
+
+study.optimize(objective, n_trials=100, show_progress_bar=True)
